2e95d445a5aaf6e0e7b51b652c00286df31ac813
Noushin <neftekhari@turing.ac.uk>
2024-08-13 10:37:25 +0100

    add loss function

--- train/train.py
+++ train/train.py
@@ -8,21 +8,35 @@ from torch.utils.data import DataLoader, random_split
 from transformers import ViTForImageClassification, AutoImageProcessor, BeitForImageClassification, BeitImageProcessor
 from tqdm import tqdm
 import os
+import wandb
 
 # Import the custom loss functions
-from tools.loss import ST_CE_loss, Bal_CE_loss, BCE_loss, LS_CE_loss, MiSLAS_loss, LADE_loss, LDAM_loss, CB_CE_loss
+from tools.loss import ST_CE_loss, Bal_CE_loss, BCE_loss, LS_CE_loss, MiSLAS_loss, LADE_loss, LDAM_loss, CB_CE_loss, \
+    MiSLAS_vit_loss, BCE_CL_loss
+
+# Initialize Weights & Biases (optional, for logging)
+# wandb.init(project='vit_finetuning')
+
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
 
 # Configuration
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 batch_size = 32
 num_workers = 16
 
 # Paths to your dataset
-train_dir = '/noc/users/noueft/Documents/corrected-data-elastic-eggs/split-train-test/train'
+train_dir = '/noc/users/noueft/Documents/New-taxonomy-model/train'
+save_path = '/noc/users/noueft/Documents/New-taxonomy-model/Code/train/model_weight'
+os.makedirs(save_path, exist_ok=True)
 
 # Image transformations with data augmentation
 transform = transforms.Compose([
-    transforms.RandomResizedCrop(224),
+    # transforms.RandomResizedCrop(224),
+    transforms.Resize(256),  # Resize smaller side to 256, maintaining aspect ratio
+    transforms.CenterCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(10),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
@@ -32,35 +46,30 @@ transform = transforms.Compose([
 
 # Load datasets
 full_train_dataset = datasets.ImageFolder(train_dir, transform=transform)
-class_counts = torch.tensor([full_train_dataset.targets.count(i) for i in range(3)])
+
+# Print the class names and their corresponding indices
+print("Class indices and labels:")
+for idx, class_name in enumerate(full_train_dataset.classes):
+    print(f"{idx}: {class_name}")
+
+class_counts = torch.tensor([full_train_dataset.targets.count(i) for i in range(18)])
 class_weights = 1. / class_counts.float()
-class_weights = class_weights / class_weights.sum() * 3  # Normalize to sum to number of classes
+class_weights = class_weights / class_weights.sum() * 18  # Normalize to sum to number of classes
 class_weights = class_weights.to(device)
 
-# Split the dataset
+# Calculate the number of samples for training and validation
 num_train = int(0.9 * len(full_train_dataset))
 num_val = len(full_train_dataset) - num_train
+
+# Split the dataset
 train_dataset, val_dataset = random_split(full_train_dataset, [num_train, num_val])
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
 
-
-# Placeholder for args needed by custom loss functions
-class Args:
-    cls_num = class_counts.tolist()
-    bal_tau = 1.0
-    smoothing = 0.1
-    nb_classes = 3
-    device = device
-
-
-args = Args()
-
-
-# Function to one-hot encode labels properly
-def one_hot_encode(labels, num_classes):
-    return torch.eye(num_classes, device=labels.device)[labels]
+# Hyperparameter grid
+learning_rates = [5e-5]
+num_epochs_options = [20, 30]
 
 
 # Function to get the loss function based on the selected flag
@@ -75,6 +84,10 @@ def get_loss_function(loss_type, class_weights, args):
         return LS_CE_loss(smoothing=args.smoothing)
     elif loss_type == 'MiSLAS':
         return MiSLAS_loss(args)
+    elif loss_type == 'MiSLAS_vit':
+        return MiSLAS_vit_loss(args)
+    elif loss_type == 'BCE_CL':
+        return BCE_CL_loss(args)
     elif loss_type == 'LADE':
         return LADE_loss(args)
     elif loss_type == 'LDAM':
@@ -85,18 +98,26 @@ def get_loss_function(loss_type, class_weights, args):
         return nn.CrossEntropyLoss()
 
 
-# Training and validation function
+def one_hot_encode(labels, num_classes):
+    return torch.eye(num_classes, device=labels.device)[labels]
+
+
 def train_and_validate(model_type, learning_rate, num_epochs, save_path, loss_type, args):
     # Load the model
     if model_type == 'vit':
-        model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224", ignore_mismatched_sizes=True,
-                                                          num_labels=3).to(device)
+        processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+        model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224", num_labels=18,
+                                                          output_attentions=True, ignore_mismatched_sizes=True).to(
+            device)
     elif model_type == 'beit':
+        processor = BeitImageProcessor.from_pretrained('microsoft/beit-base-patch16-224-pt22k-ft22k')
         model = BeitForImageClassification.from_pretrained('microsoft/beit-base-patch16-224-pt22k-ft22k',
-                                                           ignore_mismatched_sizes=True, num_labels=3).to(device)
+                                                           output_attentions=True, ignore_mismatched_sizes=True,
+                                                           num_labels=18).to(device)
     else:
         raise ValueError("Unsupported model type. Choose 'vit' or 'beit'.")
 
+    # Optimizer and loss
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = get_loss_function(loss_type, class_weights, args)
 
@@ -113,10 +134,14 @@ def train_and_validate(model_type, learning_rate, num_epochs, save_path, loss_ty
             outputs = model(images)
 
             # One-hot encode labels if needed
-            if loss_type in ['ST_CE', 'Bal_CE', 'BCE', 'LADE', 'LDAM', 'CB_CE']:
-                labels = one_hot_encode(labels, num_classes=3)
+            if loss_type in ['ST_CE', 'Bal_CE', 'BCE', 'MiSLAS', 'MiSLAS_vit', 'LADE', 'LDAM', 'CB_CE', 'BCE_CL']:
+                labels = one_hot_encode(labels, num_classes=18)
 
-            loss = loss_fn(outputs.logits, labels)
+            # Calculate loss with attention scores if applicable
+            if loss_type == 'MiSLAS_vit':
+                loss = loss_fn(outputs.logits, labels, outputs.attentions)
+            else:
+                loss = loss_fn(outputs.logits, labels)
 
             # Backward pass and optimization
             optimizer.zero_grad()
@@ -124,7 +149,9 @@ def train_and_validate(model_type, learning_rate, num_epochs, save_path, loss_ty
             optimizer.step()
 
             train_loss += loss.item()
-            train_correct += (outputs.logits.argmax(dim=-1) == labels.argmax(dim=-1)).sum().item()
+            train_correct += (outputs.logits.argmax(dim=-1) == labels.argmax(dim=-1)).sum().item() if loss_type in [
+                'ST_CE', 'Bal_CE', 'BCE', 'MiSLAS', 'MiSLAS_vit', 'LADE', 'LDAM', 'CB_CE', 'BCE_CL'] else (
+                    outputs.logits.argmax(dim=-1) == labels).sum().item()
 
         # Validation
         model.eval()
@@ -134,12 +161,20 @@ def train_and_validate(model_type, learning_rate, num_epochs, save_path, loss_ty
                 images, labels = images.to(device), labels.to(device)
                 outputs = model(images)
 
-                if loss_type in ['ST_CE', 'Bal_CE', 'BCE', 'MiSLAS', 'LADE', 'LDAM', 'CB_CE']:
-                    labels = one_hot_encode(labels, num_classes=3)
+                # One-hot encode labels if needed
+                if loss_type in ['ST_CE', 'Bal_CE', 'BCE', 'MiSLAS', 'MiSLAS_vit', 'LADE', 'LDAM', 'CB_CE', 'BCE_CL']:
+                    labels = one_hot_encode(labels, num_classes=18)
 
-                loss = loss_fn(outputs.logits, labels)
+                # Calculate loss with attention scores if applicable
+                if loss_type == 'MiSLAS_vit':
+                    loss = loss_fn(outputs.logits, labels, outputs.attentions)
+                else:
+                    loss = loss_fn(outputs.logits, labels)
+
                 val_loss += loss.item()
-                val_correct += (outputs.logits.argmax(dim=-1) == labels.argmax(dim=-1)).sum().item()
+                val_correct += (outputs.logits.argmax(dim=-1) == labels.argmax(dim=-1)).sum().item() if loss_type in [
+                    'ST_CE', 'Bal_CE', 'BCE', 'MiSLAS', 'MiSLAS_vit', 'LADE', 'LDAM', 'CB_CE', 'BCE_CL'] else (
+                        outputs.logits.argmax(dim=-1) == labels).sum().item()
 
         val_accuracy = val_correct / len(val_dataset)
         print(f"Epoch {epoch + 1}/{num_epochs}, Validation Accuracy: {val_accuracy}")
@@ -147,7 +182,7 @@ def train_and_validate(model_type, learning_rate, num_epochs, save_path, loss_ty
             best_val_accuracy = val_accuracy
             best_model_state = model.state_dict()
 
-            # Save the model
+            # Save using torch.save
             model_path = os.path.join(save_path,
                                       f"{model_type}_finetuned_{loss_type}_lr{learning_rate}_epochs{num_epochs}.pt")
             torch.save(best_model_state, model_path)
@@ -161,17 +196,23 @@ def train_and_validate(model_type, learning_rate, num_epochs, save_path, loss_ty
     return best_val_accuracy
 
 
-# Ensure the directory exists or create it
-save_path = '/noc/users/noueft/Documents/Code/PIDiff/DiffRes-PI/opt-ViT/train/model_weight'
-os.makedirs(save_path, exist_ok=True)
-
-# Grid search over hyperparameters
 best_hyperparams = None
 best_accuracy = 0
-model_type = 'beit'
-loss_type_options = ['ST_CE', 'Bal_CE', 'BCE', 'MiSLAS','LDAM', 'CB_CE']
-learning_rates = [5e-5]
-num_epochs_options = [30]
+
+loss_type_options = ['BCE_CL']
+
+
+class Args:
+    cls_num = class_counts.tolist()
+    bal_tau = 1.0
+    smoothing = 0.1
+    nb_classes = 18
+    device = device
+
+
+args = Args()
+
+model_type = 'vit'  # Change to 'beit' to use the BEiT model
 
 for loss_type in loss_type_options:
     for lr in learning_rates:
